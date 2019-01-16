51cdf31666d4fb2bdbcf2438568368c3c66476da
Adam Brenner <adam@aeb.io>
2019-01-16 00:50:09 -0800

    FIX: Check on Kristen ID
    Some parts of the code were comparing the username with discord's
    internal ID which is a bug. This fixes that.
    
    Signed-off-by: Adam Brenner <adam@aeb.io>

--- index.js
+++ index.js
@@ -55,7 +55,7 @@ client.on("message", (msg) => {
 	}
 
 	if (c.includes("good") && c.includes("night")) {
-		if (msg.author.username === KristenUserId) {
+		if (msg.author.id === KristenUserId) {
 			msg.channel.send(randomString(responses.goodNightK));
 		} else {
 			msg.channel.send(randomString(responses.goodNight));
@@ -65,7 +65,7 @@ client.on("message", (msg) => {
 	}
 
 	if (c.includes("good") && c.includes("afternoon")) {
-		if (msg.author.username === KristenUserId) {
+		if (msg.author.id === KristenUserId) {
 			msg.channel.send(randomString(responses.goodAfternoonListK));
 		} else {
 			msg.channel.send(randomString(responses.goodAfternoonList));
@@ -82,7 +82,7 @@ client.on("message", (msg) => {
 	if (
 		(c.includes("friends") || c.includes("friend")) &&
 		(c.includes("message") || c.includes("messages")) &&
-		msg.author.username === KristenUserId
+		msg.author.id === KristenUserId
 	) {
 		msg.channel.send(randomString(responses.freindMessages));
 		return;
@@ -94,7 +94,7 @@ client.on("message", (msg) => {
 		if (timeSinceKristen > twoHours) {
 			msg.channel.send(randomString(responses.missKristen));
 		} else {
-			if (msg.author.username === KristenUserId) {
+			if (msg.author.id === KristenUserId) {
 				msg.channel.send(randomString(responses.doingOkK));
 			} else {
 				msg.channel.send(randomString(responses.doingOk));
@@ -107,7 +107,7 @@ client.on("message", (msg) => {
 		c.includes("love") &&
 		c.includes("you") &&
 		c.includes(client.user.username) &&
-		msg.author.username === KristenUserId
+		msg.author.id === KristenUserId
 	) {
 		msg.channel.send(randomString(responses.iLoveYouToo));
 		return;
